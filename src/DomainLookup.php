f529cbf5504ef17a19227d02da1e907191c1582c
Thoronir42 <Thoronir42@Gmail.com>
2016-09-18 13:41:43 +0200

    Domain naming tools adjustments

--- src/DomainLookup.php
+++ src/DomainLookup.php
@@ -9,14 +9,20 @@ namespace SeStep\SettingsInterface;
  */
 trait DomainLookup
 {
-    protected function splitLocator($full_name){
+    protected function splitLocator($name, $domain = '')
+    {
+        $full_name = $name;
+        if ($domain) {
+            $full_name = $domain . '.' . $full_name;
+        }
+
         $parts = explode('.', $full_name);
-        $name = array_pop($parts);
-        $domain = implode('.', $parts);
+        $result_name = array_pop($parts);
+        $result_domain = implode('.', $parts);
 
         return [
-            'domain' => $domain,
-            'name' => $name,
+            'domain' => $result_domain,
+            'name' => $result_name,
         ];
     }
 
@@ -25,7 +31,20 @@ trait DomainLookup
         return $this->splitLocator($full_name)['domain'];
     }
 
-    protected function getNameFromFullName($full_name){
+    protected function getNameFromFullName($full_name)
+    {
         return $this->splitLocator($full_name)['name'];
+    }
+
+    protected function splitDomain($domain){
+        $parts = explode('.', $domain);
+
+        $first = array_shift($parts);
+        $rest = implode('.', $parts);
+
+        return [
+            'first' => $first,
+            'rest' => $rest,
+        ];
     }
 }
